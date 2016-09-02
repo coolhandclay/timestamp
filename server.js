c518c22cb50029ae6c0ca8bc2e8d0412e2d7ddcd
Clay <clayandrews13@gmail.com>
2016-09-02 19:24:11 +0000

    added http server and timestamp logic

--- server.js
+++ server.js
@@ -1,10 +1,39 @@
-var express = require('express');
-var app = express();
+var http = require('http');
+var url = require('url');
+var fs = require('fs');
+var port = 8080;
 
-app.get('/', function (req, res) {
-  res.send('Hello World!');
+
+var server = http.createServer(function(req, res) {
+   
+   var path = url.parse(req.url, true).path; //get the path of the request url
+   
+   if(path === '/') {
+       fs.readFile('../client/index.html', function(err, content) {
+           if(err) throw err;
+           res.end(content);
+       });
+   } else { 
+   
+   //Default case
+   var output = {date: null, unixtime: null}; //initial state of output
+   path = path.slice(1,path.length); //get just the path w/o the '/'
+   
+   //1st attempt
+   var date = new Date(path); //try first time as human-readable
+   if(date.toString() !== "Invalid Date") {
+       output = {date: date.toLocaleDateString(), unixtime: date.valueOf()};
+   } else {
+       
+       //2nd attempt
+       date = new Date(parseInt(path)); //try second time as unix
+       if(date.toString() !== "Invalid Date") {
+            output = {date: date.toLocaleDateString(), unixtime: date.valueOf()};
+       }
+   }
+   output = JSON.stringify(output);
+   res.end(output);
+   }
 });
 
-app.listen(8080, function () {
-  console.log('App listening on port 8080!');
-});
+server.listen(port);
