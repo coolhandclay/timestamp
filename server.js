c92a7b9ff2fab0a329887727464177ffaa53d965
Clay <clayandrews13@gmail.com>
2016-09-02 19:33:32 +0000

    changed port for heroku

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 var http = require('http');
 var url = require('url');
 var fs = require('fs');
-var port = 8080;
+var port = process.env.PORT || 8080;
 
 
 var server = http.createServer(function(req, res) {
